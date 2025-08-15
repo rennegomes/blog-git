1a00b78eabcf9712462ecd5300835e50724b221f
rennegomes <renne.lace082@gmail.com>
2025-08-15 17:48:44 -0300

    :lipstick: feat: Estilização CSS inicio

--- src/app/pages/inicio.tsx
+++ src/app/pages/inicio.tsx
@@ -6,8 +6,10 @@ export default function Inicio() {
     return (
         <div className="mb-24">
             <div className="flex items-end bg-[var(--base-profile)] w-full h-48"></div>
+            
+            <div className="w-1/3 m-auto h-10 blur-2xl bg-[#14589C]/80 mt-[-50px] relative z-0"></div>
 
-            <div className="w-3/4 m-auto max-w-[860px]">
+            <div className="w-3/4 m-auto max-w-[860px] relative z-20">
                 <div className="bg-[var(--base-profile)] rounded-xl shadow-2xl/50 mt-[-80px] p-8">
                     <Perfil />
                 </div>
